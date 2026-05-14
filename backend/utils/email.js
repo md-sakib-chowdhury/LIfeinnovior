8d29479826cce2f46d6d187942050a43face3af4
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-05-14 09:46:27 +0600

    back utils

---
+++ backend/utils/email.js
@@ -0,0 +1,16 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
+});
+
+exports.sendEmail = async ({ to, subject, html }) => {
+    await transporter.sendMail({
+        from: `"LifeInnovior" <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        html
+    });
+};
